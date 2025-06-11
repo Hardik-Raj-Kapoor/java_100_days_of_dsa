9c0142ac377e9acce9d2e73f2a4e4d85aca91614
Hardik-Raj-Kapoor <hardik.rajkapoor_lko@outlook.com>
2025-06-12 01:35:53 +0530

    updated the issue

--- Array_and_String/day_10_kadanes_algo.java
+++ Array_and_String/day_10_kadanes_algo.java
@@ -5,13 +5,18 @@ class kadane{
     public int kad_algo(int[] num){
         int current_sum = num[0];
         int global_sum = num[0];
-        for(int i=0;i<num.length;i++){
-            current_sum = Math.max(num[i],current_sum+num[i]);
-            global_sum = Math.max(global_sum,current_sum);
+
+        for (int i = 1; i < num.length; i++) {
+            current_sum = Math.max(num[i], current_sum + num[i]);
+            global_sum = Math.max(global_sum, current_sum);
         }
+
         return global_sum;
     }
-}
+
+
+    }
+
 public class day_10_kadanes_algo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
